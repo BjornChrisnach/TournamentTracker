24b53f809e5bad53e220707556fe6bc37b61c798
Bjorn Chrisnach <41198499+BjornChrisnach@users.noreply.github.com>
2022-02-14 17:32:52 +0100

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        // default uninitialized List, so add = new List<Person>();
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+
+        // Since c# 6.0, you can do it with the property and not more in the constructor
+        //public TeamModel()
+        //{
+        //    TeamMembers = new List<Person>();
+        //}
+
+    }
+}
